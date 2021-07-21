8a3777b7c8f689a0b69658098d58b52c45187d19
Mikhail-Bunkov <bunko04@bk.ru>
2021-07-21 12:25:58 +1000

    Spring consumer add

--- Spring-consumer/src/main/java/quoters/PostProxy.java
+++ Spring-consumer/src/main/java/quoters/PostProxy.java
@@ -1,9 +1,9 @@
 package quoters;
 
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Profiling {
-
+public @interface PostProxy {
 }
