053106caef9e31c5b7405da325886942b47a2f74
Mikhail-Bunkov <bunko04@bk.ru>
2021-07-21 12:00:52 +1000

    Spring consumer

---
+++ Spring-consumer/src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,35 @@
+package quoters;
+
+import javax.annotation.PostConstruct;
+
+@Profiling
+public class TerminatorQuoter implements Quoter{
+
+    private String message;
+
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
+
+    @Override
+    @PostConstruct
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
+
+    }
+}
