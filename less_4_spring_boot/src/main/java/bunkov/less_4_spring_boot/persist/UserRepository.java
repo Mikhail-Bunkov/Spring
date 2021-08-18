77a3ecbcc606979573cd5f254c9a29cda84fd4a9
Mikhail-Bunkov <bunko04@bk.ru>
2021-08-19 00:39:54 +1000

    homework

--- less_4_spring_boot/src/main/java/bunkov/less_4_spring_boot/persist/UserRepository.java
+++ less_4_spring_boot/src/main/java/bunkov/less_4_spring_boot/persist/UserRepository.java
@@ -1,55 +1,25 @@
 package bunkov.less_4_spring_boot.persist;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Optional;
 
-@Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
+    List<User> findByUsernameStartsWith(String prefix);
 
-    @PersistenceContext
-    private EntityManager em;
+    @Query("select u " +
+            "from User u " +
+            "where ( u.username like CONCAT(:prefix, '%') or :prefix is null ) and " +
+            "( u.age >= :minAge or :minAge is null ) and " +
+            "( u.age <= :maxAge or :maxAge is null )")
+    List<User> filterUsers(@Param("prefix") String prefix,
+                           @Param("minAge") Integer minAge,
+                           @Param("maxAge") Integer maxAge);
 
-    // Identity Map
-    private Map<Long, User> userMap = new ConcurrentHashMap<>();
-
-    private AtomicLong identity = new AtomicLong(0);
-
-    @PostConstruct
-    public void init() {
-        this.insert(new User("user1"));
-        this.insert(new User("user2"));
-        this.insert(new User("user3"));
-    }
-
-    public List<User> findAll() {
-        return new ArrayList<>(userMap.values());
-    }
-
-    public User findById(long id) {
-        return userMap.get(id);
-    }
-
-    public void insert(User user) {
-        long id = identity.incrementAndGet();
-        user.setId(id);
-        userMap.put(id, user);
-    }
-
-    public void update(User user) {
-        userMap.put(user.getId(), user);
-    }
-
-    public void delete(long id) {
-        userMap.remove(id);
-    }
-
+    Optional<User> findByUsername(String username);
 }
